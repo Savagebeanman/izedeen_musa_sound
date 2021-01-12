bde12586aa0c9ef7c15c4c9296e162291887c708
Savagebeanman <72805052+Savagebeanman@users.noreply.github.com>
2021-01-12 10:52:58 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,7 @@
+
+
+while True:
+    print ("sound level is " + input.sound_level())
+    if input.sound_level() > 150:
+        light.set.all.col
+       
---
+++ main.ts
@@ -0,0 +1,7 @@
+while (true) {
+    console.log("sound level is " + input.soundLevel())
+    if (input.soundLevel() > 150) {
+        light
+    }
+    
+}
