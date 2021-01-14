5fec36f0f0544acb7f0ad3fa3fd32f1495ac9627
Savagebeanman <72805052+Savagebeanman@users.noreply.github.com>
2021-01-14 12:54:51 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-
-
 while True:
-    print ("sound level is " + input.sound_level())
+    print ("Sound level is -" + input.sound_level())
     if input.sound_level() > 150:
-        light.set.all.col
-       
+        light.set_all(light.rgb(255,255,255))
+    else:
+       light.set_all(light.rgb(0,0,0))
--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 while (true) {
-    console.log("sound level is " + input.soundLevel())
+    console.log("Sound level is -" + input.soundLevel())
     if (input.soundLevel() > 150) {
-        light
+        light.setAll(light.rgb(255, 255, 255))
+    } else {
+        light.setAll(light.rgb(0, 0, 0))
     }
     
 }
