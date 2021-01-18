5a7935c0022e57a98ec76426ca8644cc51201574
Savagebeanman <72805052+Savagebeanman@users.noreply.github.com>
2021-01-18 18:49:41 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 #Challenge 1 
-while True:
-    print("Sound level is- " + input.sound_level())
-    if input.sound_level() > 150:
-        light.show_animation_frame(light.rainbowAnimation)
-    else:
-        light.clear()
+#while True:
+#    print("Sound level is- " + input.sound_level())
+#    if input.sound_level() > 150:
+#        light.show_animation_frame(light.rainbowAnimation)
+#        pause(3000)
+#    else:
+#        light.clear()
         
-
 #Challenge 2
-while True:
-    print("Light level is- " + input.light_level())
-    if input.light_level() > 0:
-        music.set_volume(100)
-        music.wawawawaa.play()
-    else:
-        light.clear()
+#while True:
+#    print("Light level is- " + input.light_level())
+#    if input.light_level() < 5:
+#        music.set_volume(100)
+#        music.wawawawaa.play_until_done()    
+#    else:
+#        light.clear()        
+
 #Challenge 3
+while True:
+    music.set_volume(100)
+    music.play_melody("C", 120)
--- main.ts
+++ main.ts
@@ -1,21 +1,21 @@
 // Challenge 1 
-while (true) {
-    console.log("Sound level is- " + input.soundLevel())
-    if (input.soundLevel() > 150) {
-        light.showAnimationFrame(light.rainbowAnimation)
-    } else {
-        light.clear()
-    }
-    
-}
+// while True:
+//     print("Sound level is- " + input.sound_level())
+//     if input.sound_level() > 150:
+//         light.show_animation_frame(light.rainbowAnimation)
+//         pause(3000)
+//     else:
+//         light.clear()
 // Challenge 2
+// while True:
+//     print("Light level is- " + input.light_level())
+//     if input.light_level() < 5:
+//         music.set_volume(100)
+//         music.wawawawaa.play_until_done()    
+//     else:
+//         light.clear()        
+// Challenge 3
 while (true) {
-    console.log("Light level is- " + input.lightLevel())
-    if (input.lightLevel() > 0) {
-        music.setVolume(100)
-        music.wawawawaa.play()
-    } else {
-        light.clear()
-    }
-    
+    music.setVolume(100)
+    music.playMelody("C", 120)
 }
